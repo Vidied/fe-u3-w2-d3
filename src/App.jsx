3c109929e6f5c395986596d46e2adf6089752a09
Vidied <davidepan2001@gmail.com>
2026-05-06 15:23:43 +0200

    routes per le pagine già create fatto

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,33 @@ import FooterHome from "./assets/components/FooterHome";
 import Gallery from "./assets/components/Gallery";
 import Profile from "./assets/components/Profile";
 import Settings from "./assets/components/Settings";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 function App() {
   return (
-    <div className="bg-netflix d-flex flex-column min-vh-100">
-      <HeaderHome />
-      <Profile></Profile>
-      <Settings></Settings>
-      <main className="flex-grow-1">
-        <Gallery title="Trending Now" search="Star Wars" />
-        <Gallery title="Fantasy" search="Lord of the rings" />
-        <Gallery title="Anime" search="Pokémon" />
-      </main>
-      <FooterHome />
-    </div>
+    <BrowserRouter>
+      <div className="bg-netflix d-flex flex-column min-vh-100">
+        <HeaderHome />
+
+        <main className="flex-grow-1">
+          <Routes>
+            <Route path="/Profile" element={<Profile></Profile>}></Route>
+            <Route path="/Settings" element={<Settings></Settings>}></Route>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Gallery title="Trending Now" search="Star Wars" />
+                  <Gallery title="Fantasy" search="Lord of the rings" />
+                  <Gallery title="Anime" search="Pokémon" />
+                </>
+              }
+            ></Route>
+          </Routes>
+        </main>
+        <FooterHome />
+      </div>
+    </BrowserRouter>
   );
 }
 
